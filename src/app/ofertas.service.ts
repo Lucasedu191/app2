f7847f9bd063b11985f2efd412d52c7b50d14528
Lucas Silva <lucasatletico@hotmail.com>
2020-01-16 15:20:51 -0300

    arrumando processamento do template oferta e ajustando a url da API

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,32 +2,31 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Oferta } from './shared/oferta.model'
-
+import {URL_API} from './app.api'
 @Injectable()
 
 export class OfertaService{
-
+    //private url_api = 'http://localhost:3000/ofertas'
     constructor(private http : HttpClient){}
     
     public getOfertas():Promise<Oferta[]>{
         
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(`${URL_API}?destaque=true`)
         .toPromise()
         .then((resposta : any ) => resposta)
         //retornar uma promisse oferta[]
     }
 
     public getOfertasPorCategoria(categoria: string) : Promise<Oferta[]>{
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${URL_API}?categoria=${categoria}`)
         .toPromise()
         .then((resposta: any) => resposta)
     }
-    public getOfertaPorId(id: number): Promise<Oferta>{
-        return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+    public getOfertaPorId(id: number) : Promise<Oferta>{
+        return this.http.get(`${URL_API}?id=${id}`)
         .toPromise()
         .then((resposta: any) =>{
-            console.log(resposta.shift())
-            return resposta
+            return resposta [0]
         })
 
     }
