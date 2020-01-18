d0370d944fc369892980c7b533f58109fcaaebbd
Lucas Silva <lucasatletico@hotmail.com>
2020-01-18 10:03:58 -0300

    introdução a reactive programming e observables

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router'
 import {OfertaService} from '../ofertas.service'
 import {Oferta} from '../shared/oferta.model'
+import {Observable, interval } from 'rxjs'
+
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
@@ -20,9 +22,19 @@ export class OfertaComponent implements OnInit {
   ngOnInit() {
     this.ofertasService.getOfertaPorId( this.route.snapshot.params['id'])
     .then((oferta: Oferta)=>{
-      this.oferta = oferta
-      
-    })  
+      this.oferta = oferta      
+    })
+    /*  
+    this.route.params.subscribe(
+      (parametro : any) => {console.log(parametro)},
+      (erro : any) => console.log(erro),
+      ()=> console.log('processamento foi classificado como concluido')
+    )
+    */
+    let tempo = interval(2000)
+    tempo.subscribe((intervalo : number) =>{
+      console.log(intervalo)
+    })
   }
 
 }
